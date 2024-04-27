fc65de0012f7b49235222942c7ecc5d2f0ebbb4f
Keaton Murray <murraykeaton422@gmail.com>
2024-04-27 02:40:05 -0400

    initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,5 @@
+import './bootstrap';
+import '../css/app.css';
+import './jquery-1.11.0.min.js';
+import './bootstrap.bundle.min.js';
+import './script';
---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FrontController;
+
+/*
+Route::get('/', function () {
+    return view('welcome');
+});
+*/
+
+Route::get('/', [FrontController::class, 'index'])->name('index');
